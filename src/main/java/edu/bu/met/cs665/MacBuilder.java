e073ffe3dc2a1630f0389b78c2c823650a54cbf7
paritoshshirodkar <paritoshshirodkar@gmail.com>
2019-12-05 03:02:59 -0500

    builder to build mac laptops

---
+++ src/main/java/edu/bu/met/cs665/MacBuilder.java
@@ -0,0 +1,57 @@
+package edu.bu.met.cs665;
+
+/**
+ * Concrete builder to assemble mac laptops.
+ */
+public class MacBuilder implements LaptopBuilder {
+
+  private Laptop laptop;
+
+  /**
+   * Constructor to create a MacBuilder.
+   */
+  public void MacBuilder() {
+    this.laptop = new Laptop();
+  }
+
+  @Override
+  public void addScreenSize() {
+    laptop.setScreenSize(16.0);
+  }
+
+  @Override
+  public void addOs() {
+    laptop.setOS("macOS Catalina");
+  }
+
+  @Override
+  public void addProcessor() {
+    laptop.setProcessor("Intel core i7");
+  }
+
+  @Override
+  public void addRam() {
+    laptop.setRam(32.0);
+  }
+
+  @Override
+  public void addHardDisk() {
+    laptop.setHardDisk(1000.0);
+  }
+
+  @Override
+  public void addBattery() {
+    laptop.setBattery(10000.0);
+  }
+
+  @Override
+  public void addColor() {
+    laptop.setColor("Space Gray");
+  }
+
+  @Override
+  public Laptop getLaptop() {
+    return laptop;
+  }
+
+}
