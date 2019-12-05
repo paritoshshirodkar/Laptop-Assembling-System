2a11501ad8208e7002ee18d0781578340db40e87
paritoshshirodkar <paritoshshirodkar@gmail.com>
2019-12-05 01:50:52 -0500

    basic blueprint to assemble a laptop

---
+++ src/main/java/edu/bu/met/cs665/LaptopassemblyBlueprint.java
@@ -0,0 +1,22 @@
+package edu.bu.met.cs665;
+
+/**
+ * Interface that provides the basic blueprint of assembling a laptop.
+ */
+public interface LaptopassemblyBlueprint {
+
+  public void setScreenSize(double screenSize);
+
+  public void setOS(String os);
+
+  public void setProcessor(String processor);
+
+  public void setRam(double ram);
+
+  public void setHardDisk(double hardDisk);
+
+  public void setBattery(double battery);
+
+  public void setColor(String color);
+
+}
