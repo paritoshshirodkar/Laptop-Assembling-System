2051141c44d6a77fdc68bb737936b9fff945b0be
paritoshshirodkar <paritoshshirodkar@gmail.com>
2019-12-05 02:57:41 -0500

    builder to build windows laptops

---
+++ src/main/java/edu/bu/met/cs665/WindowsBuilder.java
@@ -0,0 +1,57 @@
+package edu.bu.met.cs665;
+
+/**
+ * Concrete builder to assemble windows laptops.
+ */
+public class WindowsBuilder implements LaptopBuilder {
+
+  private Laptop laptop;
+
+  /**
+   * Constructor to create a WindowsBuilder.
+   */
+  public void WindowsBuilder() {
+    this.laptop = new Laptop();
+  }
+
+  @Override
+  public void addScreenSize() {
+    laptop.setScreenSize(15.0);
+  }
+
+  @Override
+  public void addOs() {
+    laptop.setOS("Windows 10");
+  }
+
+  @Override
+  public void addProcessor() {
+    laptop.setProcessor("Intel core i5");
+  }
+
+  @Override
+  public void addRam() {
+    laptop.setRam(16.0);
+  }
+
+  @Override
+  public void addHardDisk() {
+    laptop.setHardDisk(500.0);
+  }
+
+  @Override
+  public void addBattery() {
+    laptop.setBattery(8000.0);
+  }
+
+  @Override
+  public void addColor() {
+    laptop.setColor("White");
+  }
+
+  @Override
+  public Laptop getLaptop() {
+    return laptop;
+  }
+
+}
