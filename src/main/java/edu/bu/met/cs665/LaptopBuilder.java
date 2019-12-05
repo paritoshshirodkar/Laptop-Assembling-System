2519ab596ed300a4e3ebbb4837aedaa710f669c4
paritoshshirodkar <paritoshshirodkar@gmail.com>
2019-12-05 02:10:58 -0500

    laptop builder interface

---
+++ src/main/java/edu/bu/met/cs665/LaptopBuilder.java
@@ -0,0 +1,22 @@
+package edu.bu.met.cs665;
+
+/**
+ * Abstract interface for creating parts of a Laptop object.
+ */
+public interface LaptopBuilder {
+
+  public void addScreenSize();
+
+  public void addOs();
+
+  public void addProcessor();
+
+  public void addRam();
+
+  public void addHardDisk();
+
+  public void addBattery();
+
+  public void addColor();
+
+}
